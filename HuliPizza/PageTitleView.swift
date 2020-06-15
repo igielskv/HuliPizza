c669519531efa8466af0207df8ddd05e610fbde3
Vladislav Igielski <igielskv@icloud.com>
2020-06-15 23:28:10 +0200

    Page title subview with parameter

---
+++ HuliPizza/PageTitleView.swift
@@ -0,0 +1,24 @@
+//
+//  PageTitleView.swift
+//  HuliPizza
+//
+//  Created by Manoli on 15/06/2020.
+//  Copyright © 2020 macForce. All rights reserved.
+//
+
+import SwiftUI
+
+struct PageTitleView: View {
+    var title: String
+    
+    var body: some View {
+        Text(title)
+            .font(.largeTitle)
+    }
+}
+
+struct PageTitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageTitleView(title: "Order Pizza")
+    }
+}
