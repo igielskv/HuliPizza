da0b07978a68598c0036b92f6a2582af54633094
Vladislav Igielski <igielskv@icloud.com>
2020-06-26 09:17:52 +0200

    Adds Section to OrderListView, but delete function doesn't seem to work properly for now.

--- HuliPizza/OrderView/OrderListView.swift
+++ HuliPizza/OrderView/OrderListView.swift
@@ -13,11 +13,21 @@ struct OrderListView: View {
     
     var body: some View {
         VStack {
-            ListHeaderView(orderModel: orderModel, text: "Your Order")
-            List(orderModel.orders) { item in
-                OrderRowView(orderItem: item)
+            List {
+                Section(
+                    header: ListHeaderView(orderModel: self.orderModel, text: "Your Order")
+                ) {
+                    ForEach(orderModel.orders) { item in
+                        OrderRowView(orderItem: item)
+                    }
+                    .onDelete(perform: delete)
+                }
             }
         }
+    }
+    
+    func delete(at offsets: IndexSet) {
+        orderModel.orders.remove(atOffsets: offsets)
     }
 }
 
